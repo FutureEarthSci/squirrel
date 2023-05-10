d23b60527daafb788326178ac3657f4580078b04
Sharon Moon <sharonmoon@Sharons-MacBook-Pro.local>
2023-05-10 00:45:28 -0500

    5/10 last

--- server.js
+++ server.js
@@ -8,13 +8,14 @@ const io = new Server(server);
 const gameWidth = 1024;
 const gameHeight = 768;
 
+let joined = 0;
 
 
 class Player{
     constructor(position, velocity, playerNum){
         this.position = position;
         this.velocity = velocity;
-        this.speed = 3;
+        this.speed = 6;
         this.grounded = false;
         this.hitbox = [20,30];
         this.left = 0;
@@ -33,7 +34,7 @@ class Player{
         this.velocity[0] = (this.right - this.left) * this.speed;
         
         if (this.grounded && this.up){
-            this.velocity[1] -= 10; //constant of jump
+            this.velocity[1] -= 30; //constant of jump
         }
 
         this.position[0] += this.velocity[0];
@@ -70,7 +71,30 @@ app.get('/', (req, res) => {
 
 io.on('connection', (socket) => {
     io.to(socket.id).emit("welcome", socket.id, gameWidth, gameHeight);
+    joined+=1;
+
+    if (joined==1){
+        io.to(socket.id).emit("assignPlayerNum", 1);
+    }
+    else if (joined==2){
+        io.to(socket.id).emit("assignPlayerNum", 2);
+    }
+    else {
+        io.to(socket.id).emit("assignedPlayerNum", 0);
+    }
     
+    socket.on("updateMove", (playerNum, up_key, left_key, right_key)=>{
+        if (playerNum==1){
+            player1.left=left_key;
+            player1.right=right_key;
+            player1.up=up_key;
+        }
+        else if (playerNum==2){
+            player2.left=left_key;
+            player2.right=right_key;
+            player2.up=up_key;
+        }
+    })
 });
 
 
